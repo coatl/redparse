6c5fb4db915b2b616c4ecb114e811150d54538ec
Caleb Clausen <caleb@inforadical.net>
2010-11-04 22:28:11 -0700

    re-enable 1.9 tests

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,3 +1,3 @@
 require 'test/test_redparse'
 #require 'test/test_xform_tree' #these are broken, gyaaah, not actually used
-#require 'test/test_1.9'  #should be enabled eventually, too many broken cases for now
+require 'test/test_1.9'  #should be enabled eventually, too many broken cases for now
