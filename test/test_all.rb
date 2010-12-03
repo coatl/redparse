8cb2cc0c02506be7c599a54ce9dc06b31dc798af
Caleb Clausen <caleb@inforadical.net>
2010-12-03 14:57:06 -0800

    make tests work in 1.9 since . is not in path anymore

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,3 +1,4 @@
-require 'test/test_redparse'
-#require 'test/test_xform_tree' #these are broken, gyaaah, not actually used
-require 'test/test_1.9'  #should be enabled eventually, too many broken cases for now
+$:.push File.expand_path(File.dirname(__FILE__))
+require 'test_redparse'
+#require 'test_xform_tree' #these are broken, gyaaah, not actually used
+require 'test_1.9'  #should be enabled eventually, too many broken cases for now
