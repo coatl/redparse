ffff1ba9e5e2c9ed5613a998b3a8f6c20782e441
Caleb Clausen <caleb@inforadical.net>
2011-09-30 14:24:58 -0700

    ripper emulation, still pretty raw/imcomplete

---
+++ lib/redparse/ripper.rb
@@ -0,0 +1,376 @@
+require 'redparse'
+class RedParse
+  class Ripper
+    def initialize(text,file="(eval)",line=1,options={})
+      options[:rubyversion]||=1.9
+      @lineno=@column=0 #hack, for now. fix this later
+      @parser=RedParse.new(text,file,line,options)
+    end
+    attr_accessor :lineno,:column,:parser
+  
+    alias [] instance_variable_get
+    alias []= instance_variable_set
+
+    def parse options={}
+      @quirks=options[:quirks]
+      tree=@parser
+      tree=tree.parse if tree.respond_to? :parse
+      on_program tree.rip(self)
+    end
+
+    def quirks?; @quirks end
+
+    def self.instrumentSexpBuilder k
+      events=k.instance_methods.grep(/\Aon_/)
+      events.map!{|e| <<-"endcode" }
+        alias noinst_#{e} #{e}
+        def #{e}(*a)
+          @record<<a.dup.unshift(:#{e})
+          noinst_#{e}(*a)
+        end
+      endcode
+      events<<<<-"endcode"
+        alias noinst_parse parse
+        def parse *a
+          @record=[]
+          result=[@record,(noinst_parse *a)]
+          @record=nil
+          return result
+        end
+      endcode
+      k.module_eval events.join
+    end
+  end
+
+  class LiteralNode
+    def rip p
+      if Symbol===val
+        #p.pos huh
+        p.on_symbol_literal \
+          p.on_symbol \
+            p.on_ident(val.to_s)
+      elsif Integer===val
+        p.on_int(val.to_s)
+      else fail
+      end
+    end
+  end
+
+  class SequenceNode
+    def rip p
+      stmts_rip self,p
+    end
+  end
+
+  class CallNode
+    def rip p
+      receiver=receiver()
+      name=p.on_ident(name().dup)
+      return p.on_var_ref(name) if p.quirks? and !receiver and !params and !block and @not_real_parens
+      if receiver
+        result=[@not_real_parens ? :on_command_call : :on_call, receiver.rip(p), :".", name]
+      else
+        result=[@not_real_parens ? :on_command : :on_fcall, name]
+      end
+
+      result=p.send(*result)
+      result=p.on_method_add_arg(
+        result,
+        p.on_arg_paren(
+          p.on_args_add_block(
+            args_rip(params,p),
+            false
+      ))) if params
+
+      result=p.on_method_add_block(result,p.on_brace_block(nil,block.rip(p))) if block
+
+      return result
+    end
+  end
+
+  class OpNode
+    def rip p
+      p.on_binary(left.rip(p), op.to_sym, right.rip(p))
+    end
+  end
+  class AndNode
+    def rip p
+      p.on_binary(left.rip(p), op.to_sym, right.rip(p))
+    end
+  end
+  class OrNode
+    def rip p
+      p.on_binary(left.rip(p), op.to_sym, right.rip(p))
+    end
+  end
+
+  class UnOpNode
+    def rip p
+      p.on_unary(op.to_sym, val.rip(p))
+    end
+  end
+
+  class RescueOpNode
+    def rip p
+      p.send( "on_#{op}_mod", rescue_with.rip(p), body.rip(p) )
+    end
+  end
+
+  class WhileOpNode
+    def rip p
+      if BeginNode===consequent
+        if @quirks
+          #this is WRONG!!!, but it's how ripper works... urk
+          #if expression modified by a while operator is a begin node,
+          #ruby always executes the loop at least once, 
+          #and doesn't check the condition til after the first execution.
+          #Ripper reverses the order of condition and consequent to signal
+          #an execute-at-least-once loop. But, that's not good enough.
+          #because 'begin a end while b' now parses the same as 'b while begin a end'
+          p.on_while_mod( condition.rip(p), consequent.rip(p) ) 
+        else
+          p.on_while_mod( consequent.rip(p), condition.rip(p), :loop_first )
+        end
+      else
+        p.on_while_mod( consequent.rip(p), condition.rip(p) )
+      end
+    end
+  end
+
+  class UntilOpNode
+    def rip p
+      if BeginNode===consequent
+        if @quirks
+          #this is WRONG!!!, but it's how ripper works... urk
+          #if expression modified by a until operator is a begin node,
+          #ruby always executes the loop at least once, 
+          #and doesn't check the condition til after the first execution.
+          #Ripper reverses the order of condition and consequent to signal
+          #an execute-at-least-once loop. But, that's not good enough.
+          #because 'begin a end until b' now parses the same as 'b until begin a end'
+          p.on_until_mod( condition.rip(p), consequent.rip(p) ) 
+        else
+          p.on_until_mod( consequent.rip(p), condition.rip(p), :loop_first )
+        end
+      else
+        p.on_until_mod( consequent.rip(p), condition.rip(p) )
+      end
+    end
+  end
+
+  class IfOpNode
+    def rip p
+      p.on_if_mod( condition.rip(p), consequent.rip(p) )
+    end
+  end
+
+  class UnlessOpNode
+    def rip p
+      p.on_unless_mod( condition.rip(p), consequent.rip(p) )
+    end
+  end
+
+  class VarNode
+    def rip p
+      p.on_var_ref(
+        case name[0]
+        when ?$; p.on_gvar(name)
+        when ?@; name[1]==?@ ? p.on_cvar(name) : p.on_ivar(name)
+        when ?A..?Z; p.on_const(name)
+        else p.on_ident(name)
+        end
+      )
+    end
+  end
+
+  class ConstantNode
+    def rip p
+      if first
+        start=p.on_var_ref \
+          p.on_const(first)
+        start_i=1
+      else
+        start=p.on_top_const_ref \
+          p.on_const(self[1])
+        start_i=2
+      end
+      (start_i...size).inject(start){|sum,i| 
+        p.on_const_path_ref(sum,p.on_const(self[i])) 
+      }
+    end
+  end
+
+  class StringNode
+    def rip p
+      list=self.dup
+      list.shift if String===list.first and list.first.empty?
+      p.on_string_literal \
+        list.inject(p.on_string_content){|sum,chunk|
+          p.on_string_add sum,
+            if String===chunk
+              p.on_tstring_content chunk
+            else
+              p.on_string_embexpr chunk.rip(p)
+            end
+        }
+    end
+  end
+
+  class Node
+    def rip_and_rescues p
+        unless rescues.empty?
+          r=rescues.map{|resc| resc.rip(p)}
+          r.each_with_index{|x,i| x<<r[i+1] unless i+1==r.size }
+          r=r.first
+        end
+        p.on_bodystmt(
+          force_stmt_list_rip(body,p),
+          r,
+          else_&&else_.rip(p),
+          ensure_&&p.on_ensure(force_stmt_list_rip(ensure_,p))
+        )
+    end
+    def rip_explode! init,receiver=self,&block
+      receiver.inject(init,&block)
+    end
+    def stmts_rip list,p
+      list.inject(p.on_stmts_new){|sum,expr| p.on_stmts_add(sum,expr.rip(p)) }
+    end
+    def force_stmt_list_rip expr,p
+      if SequenceNode===expr
+        expr.rip(p)
+      else
+        stmts_rip [expr],p
+      end
+    end
+    def args_rip list,p
+      list.inject(p.on_args_new){|sum,param| 
+        p.on_args_add(sum,param.rip(p)) 
+      }
+    end
+  end
+
+  class ClassNode
+    def rip p
+      p.on_class( name.rip(p), parent&&parent.rip(p),  rip_and_rescues(p) )
+    end
+  end
+
+  class ModuleNode
+    def rip p 
+      p.on_module( name.rip(p), rip_and_rescues(p) )
+    end
+  end
+
+  class MetaClassNode
+    def rip p
+      p.on_sclass( object.rip(p), rip_and_rescues(p) )
+    end
+  end
+  
+  class MethodNode
+    def get_while params,should_be
+      param=nil
+      result=[]
+      result<<yield( param )while should_be===params.first and param=params.shift
+    ensure
+      return result unless result.empty?
+    end
+
+    def rip p
+      params=args ? args.dup : []
+      params2=[]
+      #param=nil
+
+      params2.push get_while(params,VarNode){|param| p.on_ident param.name}
+      params2.push get_while(params,AssignNode){|param| [p.on_ident(param.left.name), param.right.rip(p)]}
+      get_while(params,UnaryStarNode){|param| params2.push p.on_rest_param p.on_ident param.val.name; break }
+      params2.push get_while(params,VarNode){|param| p.on_ident param.name}
+      get_while(params,UnaryAmpNode){|param| params2.push p.on_blockarg p.on_ident param.val.name; break }
+
+      params=p.on_params( *params2 )
+      params=p.on_paren( params ) if has_parens?
+
+      result=[p.on_ident(name), params, rip_and_rescues(p)]
+      result.unshift receiver.rip(p), p.on_period(".") if receiver
+      p.on_def( *result )
+    end
+  end
+
+  class BeginNode
+    def rip p
+      p.on_begin rip_and_rescues(p)
+    end
+  end
+
+  class RescueNode
+    def rip p
+      p.on_rescue( 
+        exceptions.empty??nil:exceptions.map{|ex| ex.rip(p)}, 
+        name&&p.on_var_field(p.on_ident(name.name)), 
+        force_stmt_list_rip(action,p)
+      )
+    end
+  end
+
+  class IfNode
+    def rip p
+      elses=p.on_else(force_stmt_list_rip(otherwise,p)) if otherwise
+      elsifs.reverse_each{|ei| 
+       elses=p.on_elsif(
+          ei.condition.rip(p),
+          force_stmt_list_rip(ei.consequent,p),
+          elses
+        )
+      }
+      p.on_if(
+        condition.rip(p),
+        force_stmt_list_rip(consequent,p),
+        elses
+      )
+    end
+  end
+
+  class LoopNode
+    def rip p
+      event= @reverse ? :on_until : :on_while
+      p.send(event, condition.rip(p), force_stmt_list_rip(body,p))
+    end
+  end
+
+  class ParenedNode
+    def rip p 
+      list= SequenceNode===val ? val : [val]
+      p.on_paren stmts_rip list,p
+    end
+  end
+
+  class TernaryNode
+    def rip p
+      p.on_ifop condition.rip(p), consequent.rip(p), otherwise.rip(p)
+    end
+  end
+
+  class RangeNode
+    def rip p
+      dots= exclude_end? ? :on_dot3 : :on_dot2
+      p.send dots, left.rip(p), right.rip(p)
+    end
+  end
+
+
+  class ArrayLiteralNode
+    def rip p
+      p.on_array(args_rip(self,p))
+    end
+  end
+
+  class HashLiteralNode
+    def rip p
+      list=[]
+      (0...size).step(2){|i| list.push p.on_assoc_new(self[i].rip(p),self[i+1].rip(p)) }
+      p.on_hash((p.on_assoclist_from_args(list) unless empty?))
+    end
+  end
+
+end
---
+++ lib/redparse/ripper_sexp.rb
@@ -0,0 +1,136 @@
+#
+# $Id: sexp.rb 25189 2009-10-02 12:04:37Z akr $
+#
+# Copyright (c) 2004,2005 Minero Aoki
+#
+# This program is free software.
+# You can distribute and/or modify this program under the Ruby License.
+# For details of Ruby License, see ruby/COPYING.
+#
+
+#stolen shamelessly from ripper/sexp.rb
+#(among others. the tables came from elsewhere)
+
+class RedParse::Ripper
+
+  SCANNER_EVENT_TABLE=
+  {:CHAR=>1, :__end__=>1, :backref=>1, :backtick=>1, :comma=>1, :comment=>1, :const=>1, :cvar=>1, 
+   :embdoc=>1, :embdoc_beg=>1, :embdoc_end=>1, :embexpr_beg=>1, :embexpr_end=>1, :embvar=>1, 
+   :float=>1, :gvar=>1, :heredoc_beg=>1, :heredoc_end=>1, :ident=>1, :ignored_nl=>1, :int=>1, 
+   :ivar=>1, :kw=>1, :label=>1, :lbrace=>1, :lbracket=>1, :lparen=>1, :nl=>1, :op=>1, :period=>1, 
+   :qwords_beg=>1, :rbrace=>1, :rbracket=>1, :regexp_beg=>1, :regexp_end=>1, :rparen=>1, :semicolon=>1, 
+   :sp=>1, :symbeg=>1, :tlambda=>1, :tlambeg=>1, :tstring_beg=>1, :tstring_content=>1, :tstring_end=>1, 
+   :words_beg=>1, :words_sep=>1
+  }
+
+  PARSER_EVENT_TABLE= 
+  {:BEGIN=>1, :END=>1, :alias=>2, :alias_error=>1, :aref=>2, :aref_field=>2, :arg_ambiguous=>0, 
+   :arg_paren=>1, :args_add=>2, :args_add_block=>2, :args_add_star=>2, :args_new=>0, :array=>1, 
+   :assign=>2, :assign_error=>1, :assoc_new=>2, :assoclist_from_args=>1, :bare_assoc_hash=>1, 
+   :begin=>1, :binary=>3, :block_var=>2, :block_var_add_block=>2, :block_var_add_star=>2, :blockarg=>1, 
+   :bodystmt=>4, :brace_block=>2, :break=>1, :call=>3, :case=>2, :class=>3, :class_name_error=>1, 
+   :command=>2, :command_call=>4, :const_path_field=>2, :const_path_ref=>2, :const_ref=>1, :def=>3, 
+   :defined=>1, :defs=>5, :do_block=>2, :dot2=>2, :dot3=>2, :dyna_symbol=>1, :else=>1, :elsif=>3, 
+   :ensure=>1, :excessed_comma=>1, :fcall=>1, :field=>3, :for=>3, :hash=>1, :if=>3, :if_mod=>2, 
+   :ifop=>3, :lambda=>2, :magic_comment=>2, :massign=>2, :method_add_arg=>2, :method_add_block=>2, 
+   :mlhs_add=>2, :mlhs_add_star=>2, :mlhs_new=>0, :mlhs_paren=>1, :module=>2, :mrhs_add=>2, 
+   :mrhs_add_star=>2, :mrhs_new=>0, :mrhs_new_from_args=>1, :next=>1, :opassign=>3, 
+   :operator_ambiguous=>2, :param_error=>1, :params=>5, :paren=>1, :parse_error=>1, :program=>1, 
+   :qwords_add=>2, :qwords_new=>0, :redo=>0, :regexp_add=>2, :regexp_literal=>2, :regexp_new=>0, 
+   :rescue=>4, :rescue_mod=>2, :rest_param=>1, :retry=>0, :return=>1, :return0=>0, :sclass=>2, 
+   :stmts_add=>2, :stmts_new=>0, :string_add=>2, :string_concat=>2, :string_content=>0, 
+   :string_dvar=>1, :string_embexpr=>1, :string_literal=>1, :super=>1, :symbol=>1, :symbol_literal=>1, 
+   :top_const_field=>1, :top_const_ref=>1, :unary=>2, :undef=>1, :unless=>3, :unless_mod=>2, 
+   :until=>2, :until_mod=>2, :var_alias=>2, :var_field=>1, :var_ref=>1, :void_stmt=>0, :when=>3, 
+   :while=>2, :while_mod=>2, :word_add=>2, :word_new=>0, :words_add=>2, :words_new=>0, 
+   :xstring_add=>2, :xstring_literal=>1, :xstring_new=>0, :yield=>1, :yield0=>0, :zsuper=>0
+  } 
+
+  PARSER_EVENTS=PARSER_EVENT_TABLE.keys
+  SCANNER_EVENTS=SCANNER_EVENT_TABLE.keys
+
+  # [EXPERIMENTAL]
+  # Parses +src+ and create S-exp tree.
+  # This method is for mainly developper use.
+  #
+  #   require 'ripper'
+  #   require 'pp
+  #
+  #   pp Ripper.sexp("def m(a) nil end")
+  #     #=> [:program,
+  #          [:stmts_add,
+  #           [:stmts_new],
+  #           [:def,
+  #            [:@ident, "m", [1, 4]],
+  #            [:paren, [:params, [[:@ident, "a", [1, 6]]], nil, nil, nil]],
+  #            [:bodystmt,
+  #             [:stmts_add, [:stmts_new], [:var_ref, [:@kw, "nil", [1, 9]]]],
+  #             nil,
+  #             nil,
+  #             nil]]]]
+  #
+  def self.sexp(src, filename = '-', lineno = 1)
+    SexpBuilderPP.new(src, filename, lineno).parse
+  end
+
+  def self.sexp_raw(src, filename = '-', lineno = 1)
+    SexpBuilder.new(src, filename, lineno).parse
+  end
+
+  class SexpBuilderPP < ::RedParse::Ripper   #:nodoc:
+    private
+
+    PARSER_EVENT_TABLE.each do |event, arity|
+      if /_new\z/ =~ event.to_s and arity == 0
+        module_eval(<<-End, __FILE__, __LINE__ + 1)
+          def on_#{event}
+            []
+          end
+        End
+      elsif /_add\z/ =~ event.to_s
+        module_eval(<<-End, __FILE__, __LINE__ + 1)
+          def on_#{event}(list, item)
+            list.push item
+            list
+          end
+        End
+      else
+        module_eval(<<-End, __FILE__, __LINE__ + 1)
+          def on_#{event}(*args)
+            [:#{event}, *args]
+          end
+        End
+      end
+    end
+
+    SCANNER_EVENTS.each do |event|
+      module_eval(<<-End, __FILE__, __LINE__ + 1)
+        def on_#{event}(tok)
+          [:@#{event}, tok, [lineno(), column()]]
+        end
+      End
+    end
+  end
+
+  class SexpBuilder < ::RedParse::Ripper   #:nodoc:
+    private
+
+    PARSER_EVENTS.each do |event|
+      module_eval(<<-End, __FILE__, __LINE__ + 1)
+        def on_#{event}(*args)
+          args.unshift :#{event}
+          args
+        end
+      End
+    end
+
+    SCANNER_EVENTS.each do |event|
+      module_eval(<<-End, __FILE__, __LINE__ + 1)
+        def on_#{event}(tok)
+          [:@#{event}, tok, [lineno(), column()]]
+        end
+      End
+    end
+  end
+
+end
